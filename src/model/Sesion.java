007d70052d46337419740d355827a89b9dd34444
aacadavid <aacadavid@unicesar.edu.co>
2026-05-18 20:33:24 -0500

    feat: diseño de clase sesion

--- src/model/Sesion.java
+++ src/model/Sesion.java
@@ -4,10 +4,86 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author alvar
  */
 public class Sesion {
+    
+    private int identificador;
+    private LocalDate fechaRealizacion;
+    private double duracionHoras;
+    private Artista artista;
+    private Productor productor;
+    private double costoTotal;
+
+    public Sesion() {
+    }
+
+    public Sesion(int identificador, LocalDate fechaRealizacion, double duracionHoras, Artista artista, Productor productor, double costoTotal) {
+        this.identificador = identificador;
+        this.fechaRealizacion = fechaRealizacion;
+        this.duracionHoras = duracionHoras;
+        this.artista = artista;
+        this.productor = productor;
+        this.costoTotal = costoTotal;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public LocalDate getFechaRealizacion() {
+        return fechaRealizacion;
+    }
+
+    public void setFechaRealizacion(LocalDate fechaRealizacion) {
+        this.fechaRealizacion = fechaRealizacion;
+    }
+
+    public double getDuracionHoras() {
+        return duracionHoras;
+    }
+
+    public void setDuracionHoras(double duracionHoras) {
+        this.duracionHoras = duracionHoras;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    public Productor getProductor() {
+        return productor;
+    }
+
+    public void setProductor(Productor productor) {
+        this.productor = productor;
+    }
+
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+
+    public void setCostoTotal(double costoTotal) {
+        this.costoTotal = costoTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "Sesion{" + "identificador=" + identificador + ", fechaRealizacion=" + fechaRealizacion + ", duracionHoras=" + duracionHoras + ", artista=" + artista + ", productor=" + productor + ", costoTotal=" + costoTotal + '}';
+    }
+    
+    
     
 }
