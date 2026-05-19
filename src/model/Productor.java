eb5e7791cebfe59a5e94304de13dc78da79837f2
aacadavid <aacadavid@unicesar.edu.co>
2026-05-18 20:25:03 -0500

    feat: diseño de clase productor

--- src/model/Productor.java
+++ src/model/Productor.java
@@ -8,7 +8,44 @@ package model;
  *
  * @author alvar
  */
-public class Productor {
+public class Productor extends Persona{
+    
+    private String especialidad;
+    private double tarifaHora;
+
+    public Productor() {
+    }
+
+    public Productor(String especialidad, double tarifaHora, int identificacion, String nombre, String correo, String telefono) {
+        super(identificacion, nombre, correo, telefono);
+        this.especialidad = especialidad;
+        this.tarifaHora = tarifaHora;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + getIdentificacion() + 
+               ", Nombre: " + getNombre() + 
+               ", Correo: " + getCorreo() + 
+               ", Telefono: " + getTelefono() + 
+               ", Especialidad: " + especialidad;
+    }
+    
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public double getTarifaHora() {
+        return tarifaHora;
+    }
+
+    public void setTarifaHora(double tarifaHora) {
+        this.tarifaHora = tarifaHora;
+    }
     
     
     
