693eb93d8188f4b3214232da12d913e8deb35ea7
aacadavid <aacadavid@unicesar.edu.co>
2026-05-18 20:47:38 -0500

    diseño de clase cancion

--- src/model/Cancion.java
+++ src/model/Cancion.java
@@ -10,4 +10,66 @@ package model;
  */
 public class Cancion {
     
+   private int identificador;
+   private String titulo;
+   private double duracionMinutos;
+   private String genero;
+   private Artista artista;
+
+    public Cancion() {
+    }
+
+    public Cancion(int identificador, String titulo, double duracionMinutos, String genero, Artista artista) {
+        this.identificador = identificador;
+        this.titulo = titulo;
+        this.duracionMinutos = duracionMinutos;
+        this.genero = genero;
+        this.artista = artista;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(double duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "identificador=" + identificador + ", titulo=" + titulo + ", duracionMinutos=" + duracionMinutos + ", genero=" + genero + ", artista=" + artista + '}';
+    }
+    
 }
