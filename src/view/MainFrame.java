af76be9b7e60d9fa31472210048c1d95692d08e9
aacadavid <aacadavid@unicesar.edu.co>
2026-05-23 12:25:55 -0500

    feat: refactorización de clase formCatalogo para adaptar a modelo relacional con incorporacion de agregacion de generos

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -58,7 +58,7 @@ public class MainFrame extends JFrame {
         JPanel statusBar = construirStatusBar();
         root.add(statusBar, BorderLayout.SOUTH);
         setContentPane(root);
-    }
+    }   
     // =================================================================
     // SIDEBAR
     // =================================================================
